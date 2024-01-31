5b59ef63a2bb79b78a5796222368b3091e01a852
ChemsCode <saidiceb@gmail.com>
2024-01-30 19:01:48 -0500

    inital setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('preline/plugin'),
+  ],
+}
+
