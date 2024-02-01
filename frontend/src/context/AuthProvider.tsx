1c78d2348c40ac540ec0b4b65900841dcf10fd0f
ChemsCode <saidiceb@gmail.com>
2024-02-01 10:44:22 -0500

    added authprovider global context

---
+++ frontend/src/context/AuthProvider.tsx
@@ -0,0 +1,34 @@
+import { createContext, useState, ReactNode } from "react";
+
+interface AuthContextProps {
+    auth: any;
+    setAuth: (auth: any) => void;
+    persist: boolean;
+    setPersist: (persist: boolean) => void;
+}
+
+const AuthContext = createContext<AuthContextProps>({
+    auth: {},
+    setAuth: () => {},
+    persist: false,
+    setPersist: () => {},
+});
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+    const [auth, setAuth] = useState({});
+    const [persist, setPersist] = useState(
+        JSON.parse(localStorage.getItem("persist") ?? "false")
+    );
+
+    return (
+        <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
