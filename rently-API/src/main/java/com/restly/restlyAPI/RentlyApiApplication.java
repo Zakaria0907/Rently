d461ee9c2a3bfc27b6311a5bf95e6b951dfd7f44
KA-DEVL <anes1999@live.ca>
2024-02-02 18:40:52 -0500

    Implemetation of User entity and user related activityes & roles

--- rently-API/src/main/java/com/restly/restlyAPI/RentlyApiApplication.java
+++ rently-API/src/main/java/com/restly/restlyAPI/RentlyApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RentlyApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestlyApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RentlyApiApplication.class, args);
+    }
 
 }
---
+++ rently-API/src/main/java/com/restly/restlyAPI/user/Role.java
@@ -0,0 +1,54 @@
+package com.restly.restlyAPI.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+  //User currently have no permission for protected resources
+  USER(Collections.emptySet()),
+
+  //Admin have all permissions
+  ADMIN(
+          Set.of(
+                  ADMIN_READ,
+                  ADMIN_UPDATE,
+                  ADMIN_DELETE,
+                  ADMIN_CREATE,
+                  MANAGER_READ,
+                  MANAGER_UPDATE,
+                  MANAGER_DELETE,
+                  MANAGER_CREATE
+          )
+  ),
+  MANAGER(
+          Set.of(
+                  MANAGER_READ,
+                  MANAGER_UPDATE,
+                  MANAGER_DELETE,
+                  MANAGER_CREATE
+          )
+  )
+
+  ;
+
+  private final Set<Permission> permissions;
+
+  public List<SimpleGrantedAuthority> getAuthorities() {
+    var authorities = getPermissions()
+            .stream()
+            .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+            .collect(Collectors.toList());
+    authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    return authorities;
+  }
+}
