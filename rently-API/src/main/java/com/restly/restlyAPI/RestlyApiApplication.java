6d3120f9e6bf7e96e9f2435f8180aec363b2c395
KA-DEVL <anes1999@live.ca>
2024-02-01 23:28:22 -0500

    Fixing project name typo and adding comment on application yml

--- rently-API/src/main/java/com/restly/restlyAPI/RestlyApiApplication.java
+++ rently-API/src/main/java/com/restly/restlyAPI/RestlyApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestlyApiApplication {
+public class RentlyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestlyApiApplication.class, args);
