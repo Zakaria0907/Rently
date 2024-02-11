a7d4039958fc2243e9925dae511ed430eb0a4ba6
KA-DEVL <anes1999@live.ca>
2024-02-11 14:03:17 -0500

    REN-46 : Adding header and footer in Landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./pages/Layout";
 import Landing from "./components/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,9 +1,11 @@
 import tw from "tailwind-styled-components"
 import React, { useRef } from 'react';
-import Hero from "./Hero";
+import Hero from "../pages/Hero";
 import Testimonials from "./Testimonials";
 import Features from "./Features";
 import { useIsVisible } from "../hooks/useIsVisible";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Landing: React.FC = () => {
 
@@ -18,6 +20,7 @@ const Landing: React.FC = () => {
 
     return (
         <>
+        <Navbar />
             <div ref={ref1} className={`transition-opacity ease-in duration-1000 ${isVisible1 ? "opacity-100" : "opacity-0"}`}>
                 <Hero />
             </div>
@@ -27,6 +30,7 @@ const Landing: React.FC = () => {
             <div ref={ref3} className={`transition-opacity ease-in duration-1000 ${isVisible3 ? "opacity-100" : "opacity-0"}`}>
                 <Features />
             </div>
+        <Footer />
         </>
 
     );
