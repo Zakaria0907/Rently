0c5d1d96159c2fb2edc735308faf945105befd34
Zakaria <zackelmaoo@hotmail.fr>
2024-02-26 01:30:16 -0500

    REN-91
    added comments

--- rently-API/src/main/java/com/rently/rentlyAPI/controller/CompanyController.java
+++ rently-API/src/main/java/com/rently/rentlyAPI/controller/CompanyController.java
@@ -20,27 +20,27 @@ public class CompanyController {
 
     private final CompanyService companyService;
     
-    
+    // Endpoint to get a building by companyId and buildingId
     @GetMapping("/{companyId}/building/{buildingId}")
     public ResponseEntity<BuildingDto> getBuildingByCompanyIdAndBuildingId(@PathVariable("companyId") Integer companyId, @PathVariable("buildingId") Integer buildingId) {
         BuildingDto building = companyService.getBuildingByCompanyIdAndBuildingId(companyId, buildingId);
         return ResponseEntity.ok(building);
     }
     
-    
+    // Endpoint to get all buildings by companyId
     @GetMapping("/building/all/{companyId}")
     public ResponseEntity<List<BuildingDto>> getAllBuildingsByCompanyId(@PathVariable("companyId") Integer companyId) {
         List<BuildingDto> buildings = companyService.getAllBuildingsByCompanyId(companyId);
         return ResponseEntity.ok(buildings);
     }
     
-    
+    // Endpoint to create a building by companyId
     @PostMapping("/create-building/{company-id}" )
     public ResponseEntity<BuildingDto> createBuilding(@PathVariable (value = "company-id") Integer companyId, @RequestBody BuildingDto buildingDto) {
         return ResponseEntity.ok(companyService.createBuildingByCompanyId(companyId, buildingDto));
     }
     
-
+    //TODO: modify the endpoint to associate the condo with the building
     @PostMapping("/create-condo/{company-id}" )
     public ResponseEntity<CondoDto> createCondo(@PathVariable (value = "company-id") Integer companyId, @RequestBody CondoDto condoDto) {
         return ResponseEntity.ok(companyService.createCondoByCompanyId(companyId, condoDto));
