6cf57175a4b135176f14b4c98cd99dcbeb0b6dfa
Adam Boucher <adampboucher@gmail.com>
2024-02-27 16:14:23 -0500

    Update CompanyControllerTest.java
    two more passing tests

--- rently-API/src/test/java/com/rently/rentlyAPI/controller/CompanyControllerTest.java
+++ rently-API/src/test/java/com/rently/rentlyAPI/controller/CompanyControllerTest.java
@@ -1,11 +1,14 @@
 package com.rently.rentlyAPI.controller;
 
 import com.rently.rentlyAPI.dto.BuildingDto;
+import com.rently.rentlyAPI.dto.CondoDto;
 import com.rently.rentlyAPI.services.CompanyService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -17,11 +20,12 @@ public class CompanyControllerTest {
     CompanyService mockedCompanyService = mock(CompanyService.class);
     int companyId = 1, buildingId = 2;
 
+    CompanyController testController = new CompanyController(mockedCompanyService);
+
     @Test
     public void testCreateBuilding() {
         // Arrange
         when(mockedCompanyService.createBuildingByCompanyId(companyId, mockedBuildingDto)).thenReturn(mockedBuildingDto);
-        CompanyController testController = new CompanyController(mockedCompanyService);
 
 
         // Act
@@ -38,7 +42,6 @@ public class CompanyControllerTest {
     public void testGetBuildingCompanyIdAndBuildingId() {
         // Arrange
         when(mockedCompanyService.getBuildingByCompanyIdAndBuildingId(companyId, buildingId)).thenReturn(mockedBuildingDto);
-        CompanyController testController = new CompanyController(mockedCompanyService);
 
 
         // Act
@@ -49,5 +52,60 @@ public class CompanyControllerTest {
         assertEquals(HttpStatus.OK, testResponseEntity.getStatusCode());
         assertEquals(ResponseEntity.ok(mockedBuildingDto), testResponseEntity);
         assertEquals(mockedBuildingDto, testResponseEntity.getBody()); // not sure if this is needed, leaving it in for now
+    }
+
+//    @Test
+//    public void testUpdateBuildingByCompanyIdAndBuildingId() {
+//        // Arrange
+//        when(mockedCompanyService.getBuildingByCompanyIdAndBuildingId(companyId, buildingId)).thenReturn(mockedBuildingDto);
+//        CompanyController testController = new CompanyController(mockedCompanyService);
+//
+//
+//        // Act
+//        ResponseEntity<BuildingDto> testResponseEntity = testController.getBuildingByCompanyIdAndBuildingId(companyId, buildingId);
+//
+//
+//        // Assert
+//        assertEquals(HttpStatus.OK, testResponseEntity.getStatusCode());
+//        assertEquals(ResponseEntity.ok(mockedBuildingDto), testResponseEntity);
+//        assertEquals(mockedBuildingDto, testResponseEntity.getBody()); // not sure if this is needed, leaving it in for now
+//    }
+
+    @Test
+    public void testGetAllBuildingsByCompanyId() {
+        // Arrange
+
+        //mock
+        List<BuildingDto> mockedList = mock(List.class);
+
+        when(mockedCompanyService.getAllBuildingsByCompanyId(companyId)).thenReturn(mockedList);
+
+
+        // Act
+        ResponseEntity<List<BuildingDto>> testResponseEntity = testController.getAllBuildingsByCompanyId(companyId);
+
+
+        // Assert
+        assertEquals(HttpStatus.OK, testResponseEntity.getStatusCode());
+        assertEquals(ResponseEntity.ok(mockedList), testResponseEntity);
+    }
+
+    @Test
+    public void testCreateCondo() {
+        // Arrange
+
+        //mock
+        CondoDto mockedCondoDto = mock(CondoDto.class);
+
+        when(mockedCompanyService.createCondoByCompanyId(companyId, buildingId, mockedCondoDto)).thenReturn(mockedCondoDto);
+
+
+        // Act
+        ResponseEntity<CondoDto> testResponseEntity = testController.createCondo(companyId, buildingId, mockedCondoDto);
+
+
+        // Assert
+        assertEquals(HttpStatus.OK, testResponseEntity.getStatusCode());
+        assertEquals(ResponseEntity.ok(mockedCondoDto), testResponseEntity);
     }
 }
