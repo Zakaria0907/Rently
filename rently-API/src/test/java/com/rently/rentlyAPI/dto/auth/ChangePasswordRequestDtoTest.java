6ba13ed5187e6302e19a8f33bddb91ddafd39622
Adam Boucher <adampboucher@gmail.com>
2024-02-28 11:57:27 -0500

    REN-113 Create ChangePasswordRequestDtoTest.java
    all tests pass

---
+++ rently-API/src/test/java/com/rently/rentlyAPI/dto/auth/ChangePasswordRequestDtoTest.java
@@ -0,0 +1,57 @@
+package com.rently.rentlyAPI.dto.auth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ChangePasswordRequestDtoTest {
+
+    @Test
+    public void testAllArgsConstructor() {
+        // Arrange
+
+
+        // Act
+        ChangePasswordRequestDto testChangePasswordRequestDto = new ChangePasswordRequestDto("123", "456", "789");
+
+        // Assert
+        assertEquals("123", testChangePasswordRequestDto.getCurrentPassword());
+        assertEquals("456", testChangePasswordRequestDto.getNewPassword());
+        assertEquals("789", testChangePasswordRequestDto.getConfirmationPassword());
+    }
+
+    @Test
+    public void testBuilderConstructor() {
+        // Arrange
+
+
+        // Act
+        ChangePasswordRequestDto testChangePasswordRequestDto = ChangePasswordRequestDto.builder().build();
+
+        // Assert
+        assertNull(testChangePasswordRequestDto.getCurrentPassword());
+        assertNull(testChangePasswordRequestDto.getNewPassword());
+        assertNull(testChangePasswordRequestDto.getConfirmationPassword());
+
+    }
+
+    @Test
+    public void testSetters() {
+        // Arrange
+
+
+        // Act
+        ChangePasswordRequestDto testChangePasswordRequestDto = ChangePasswordRequestDto.builder().build();
+        testChangePasswordRequestDto.setCurrentPassword("bing");
+        testChangePasswordRequestDto.setNewPassword("bong");
+        testChangePasswordRequestDto.setConfirmationPassword("shling");
+
+        // Assert
+        assertEquals("bing", testChangePasswordRequestDto.getCurrentPassword());
+        assertEquals("bong", testChangePasswordRequestDto.getNewPassword());
+        assertEquals("shling", testChangePasswordRequestDto.getConfirmationPassword());
+
+    }
+    
+}
