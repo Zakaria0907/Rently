113c5b42d4abfa5756e5361139cb401381b41927
Adam Boucher <adampboucher@gmail.com>
2024-02-28 12:49:58 -0500

    REN-113 Create RefreshTokenTest.java
    all tests pass

---
+++ rently-API/src/test/java/com/rently/rentlyAPI/entity/auth/RefreshTokenTest.java
@@ -0,0 +1,67 @@
+package com.rently.rentlyAPI.entity.auth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+
+public class RefreshTokenTest {
+
+
+    AccessToken mockedAccessToken = mock(AccessToken.class);
+    @Test
+    public void testAllArgsConstructor() {
+        // Arrange
+
+
+        // Act
+        RefreshToken testRefreshToken = new RefreshToken(1, "123abc", TokenType.BEARER, true, true, mockedAccessToken);
+
+        // Assert
+        assertEquals(1, testRefreshToken.getId());
+        assertEquals("123abc", testRefreshToken.getRefreshToken());
+        assertEquals(TokenType.BEARER, testRefreshToken.getTokenType());
+        assertTrue(testRefreshToken.isRevoked());
+        assertTrue(testRefreshToken.isExpired());
+        assertEquals(mockedAccessToken, testRefreshToken.getAccessToken());
+
+    }
+
+    @Test
+    public void testNoArgsConstructor() {
+        // Arrange
+
+
+        // Act
+        RefreshToken testRefreshToken = new RefreshToken();
+
+        // Assert
+        assertNull(testRefreshToken.getId());
+        assertNull(testRefreshToken.getRefreshToken());
+        assertEquals(TokenType.BEARER, testRefreshToken.getTokenType());
+        assertFalse(testRefreshToken.isRevoked());
+        assertFalse(testRefreshToken.isExpired());
+        assertNull(testRefreshToken.getAccessToken());
+
+    }
+
+    @Test
+    public void testBuilder() {
+        // Arrange
+
+
+        // Act
+        RefreshToken testRefreshToken = RefreshToken.builder().build();
+
+        // Assert
+        assertNull(testRefreshToken.getId());
+        assertNull(testRefreshToken.getRefreshToken());
+        assertNull(testRefreshToken.getTokenType());
+        assertFalse(testRefreshToken.isRevoked());
+        assertFalse(testRefreshToken.isExpired());
+        assertNull(testRefreshToken.getAccessToken());
+
+    }
+
+}
