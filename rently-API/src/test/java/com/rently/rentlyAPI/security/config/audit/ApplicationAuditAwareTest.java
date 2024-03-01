87fa89f56700f1f0d64508e46b8475515353fabb
Adam Boucher <adampboucher@gmail.com>
2024-02-29 21:13:13 -0500

    REN-113 ApplicationAuditAwareTest
    removed unused mocks and

--- rently-API/src/test/java/com/rently/rentlyAPI/security/config/audit/ApplicationAuditAwareTest.java
+++ rently-API/src/test/java/com/rently/rentlyAPI/security/config/audit/ApplicationAuditAwareTest.java
@@ -1,25 +1,17 @@
 package com.rently.rentlyAPI.security.config.audit;
 
-import com.rently.rentlyAPI.entity.User;
-import org.springframework.security.core.Authentication;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ApplicationAuditAwareTest {
 
-    Authentication mockedAuthentication = mock(Authentication.class);
-    Object mockedObject = mock(Object.class);
     @Test
     public void testGetCurrentAuditor() {
         // Arrange
         ApplicationAuditAware testApplicationAuditAware = new ApplicationAuditAware();
-        when(mockedAuthentication.getPrincipal()).thenReturn(mockedObject);
 
         // Act
         Optional<Integer> testOptionalInteger = testApplicationAuditAware.getCurrentAuditor();
