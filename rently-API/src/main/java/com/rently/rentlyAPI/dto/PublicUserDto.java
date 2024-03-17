ac32f36aea1c18aff52b4faf6ee8be3e5defbcc9
Oussama.C <oussamacherifi7@gmail.com>
2024-03-17 15:26:44 -0400

    added PublicUserDto.java

---
+++ rently-API/src/main/java/com/rently/rentlyAPI/dto/PublicUserDto.java
@@ -0,0 +1,79 @@
+package com.rently.rentlyAPI.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.rently.rentlyAPI.entity.user.PublicUser;
+import com.rently.rentlyAPI.security.Role;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+public class PublicUserDto {
+
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("email")
+    @NotBlank(message = "The email is required")
+    private String email;
+
+    @JsonProperty("password")
+    @NotBlank(message = "The password name is required")
+    private String password;
+
+    @JsonProperty("first_name")
+    @NotBlank(message = "The first name is required")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    @NotBlank(message = "The last name is required")
+    private String lastName;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    @JsonProperty("bio")
+    private String bio;
+
+    @JsonProperty("role")
+    @Builder.Default
+    private String role = Role.PUBLIC_USER.name();
+
+    @JsonProperty("registration_key")
+    private String registrationKey;
+
+    public PublicUser toEntity(PublicUserDto publicUserDto){
+        return PublicUser.builder()
+                .email(publicUserDto.getEmail())
+                .password(publicUserDto.getPassword())
+                .firstName(publicUserDto.getFirstName())
+                .lastName(publicUserDto.getLastName())
+                .phoneNumber(publicUserDto.getPhoneNumber())
+                .bio(publicUserDto.getBio())
+                .role(Role.valueOf(publicUserDto.getRole()))
+                .registrationKey(publicUserDto.getRegistrationKey())
+                // companyId is set in business logic
+                .build();
+
+    }
+
+    public PublicUserDto fromEntity(PublicUser publicUser){
+        return PublicUserDto.builder()
+                .id(publicUser.getId())
+                .email(publicUser.getEmail())
+                .firstName(publicUser.getFirstName())
+                .lastName(publicUser.getLastName())
+                .phoneNumber(publicUser.getPhoneNumber())
+                .bio(publicUser.getBio())
+                .role(publicUser.getRole().name())
+                .registrationKey(publicUser.getRegistrationKey())
+                .build();
+    }
+
+
+
+}
