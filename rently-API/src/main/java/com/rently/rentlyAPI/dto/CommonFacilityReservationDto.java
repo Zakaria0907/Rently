064a322ce531825c820d8b98dcb157136b39a5d8
Oussama.C <oussamacherifi7@gmail.com>
2024-03-19 15:17:47 -0400

    REN-59 implementing CommonFacilityReservationDto

--- rently-API/src/main/java/com/rently/rentlyAPI/dto/CommonFacilityReservationDto.java
+++ rently-API/src/main/java/com/rently/rentlyAPI/dto/CommonFacilityReservationDto.java
@@ -1,4 +1,49 @@
 package com.rently.rentlyAPI.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.rently.rentlyAPI.entity.CommonFacility;
+import com.rently.rentlyAPI.entity.CommonFacilityReservation;
+import com.rently.rentlyAPI.entity.Company;
+import com.rently.rentlyAPI.entity.user.Occupant;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+@AllArgsConstructor
 public class CommonFacilityReservationDto {
+
+    @JsonProperty("company_id")
+    @NotBlank(message = "The company id is required")
+    private Company company;
+
+    @JsonProperty("common_facility_id")
+    @NotBlank(message = "The email is required")
+    private CommonFacility commonFacility;
+
+    @JsonProperty("occupant_id")
+    @NotBlank(message = "The occupant id is required")
+    private Occupant occupant;
+
+    public static CommonFacilityReservation toEntity(CommonFacilityReservationDto commonFacilityReservationDto){
+        return com.rently.rentlyAPI.entity.CommonFacilityReservation.builder()
+                .company(commonFacilityReservationDto.getCompany())
+                .commonFacility(commonFacilityReservationDto.getCommonFacility())
+                .occupant(commonFacilityReservationDto.getOccupant())
+                .build();
+
+    }
+
+    public static CommonFacilityReservationDto fromEntity(CommonFacilityReservation commonFacilityReservation){
+        return CommonFacilityReservationDto.builder()
+                .company(commonFacilityReservation.getCompany())
+                .commonFacility(commonFacilityReservation.getCommonFacility())
+                .occupant(commonFacilityReservation.getOccupant())
+                .build();
+    }
 }
+
+
