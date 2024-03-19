b59b04a102c51201ca730024637178e5b4263ad7
ChemsCode <saidiceb@gmail.com>
2024-03-19 00:01:35 -0400

    fixed a bug

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom"
 // import Navbar from "./Navbar"
 // import Footer from "./Footer"
-import { useCart } from "../context/CartContext";
+// import { useCart } from "../context/CartContext";
 // import SideBar from "./Sidebar v2/SideBar";
 
 const Layout = () => {
-    const { isCartVisible } = useCart(); // Use the useCart hook to get the cart visibility state
+    // const { isCartVisible } = useCart(); // Use the useCart hook to get the cart visibility state
 
     return (
         <>
             <div className="flex flex-col min-h-screen">
                 {/* <Navbar /> */}
                 <div className="flex flex-1">
-                    <div className={`flex-1 ${isCartVisible ? 'lg:w-11/12' : 'w-full'}`}>
+                    <div className={`flex-1 ${'w-full'}`}>
                         <Outlet/>
                     </div>
                     {/* <SideBar/> */}
