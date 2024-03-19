e5df2e063a064e82bd054e651db5c94855411eac
Oussama.C <oussamacherifi7@gmail.com>
2024-03-19 16:22:51 -0400

    REN-59 added create method to CommonFacilityReservationImpl

--- rently-API/src/main/java/com/rently/rentlyAPI/services/impl/CommonFacilityReservationImpl.java
+++ rently-API/src/main/java/com/rently/rentlyAPI/services/impl/CommonFacilityReservationImpl.java
@@ -2,21 +2,43 @@ package com.rently.rentlyAPI.services.impl;
 
 import com.rently.rentlyAPI.dto.CommonFacilityReservationDto;
 import com.rently.rentlyAPI.dto.PublicUserDto;
+import com.rently.rentlyAPI.entity.CommonFacilityReservation;
+import com.rently.rentlyAPI.entity.user.PublicUser;
+import com.rently.rentlyAPI.exceptions.OperationNonPermittedException;
+import com.rently.rentlyAPI.repository.CommonFacilityReservationRepository;
+import com.rently.rentlyAPI.repository.CompanyRepository;
 import com.rently.rentlyAPI.services.CommonFacilityReservationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class CommonFacilityReservationImpl implements CommonFacilityReservationService{
+
+    private final CommonFacilityReservationRepository commonFacilityReservationRepository;
+
     @Override
-    public PublicUserDto createReservation(CommonFacilityReservationDto commonFacilityReservationDto) {
+    public CommonFacilityReservationDto createReservation(CommonFacilityReservationDto commonFacilityReservationDto) {
+        //check the user does not already exist
+        Optional<CommonFacilityReservation> existing = commonFacilityReservationRepository.findByOccupantId(commonFacilityReservationDto.getOccupant().getId(), commonFacilityReservationDto.getCommonFacility().getId());
+
+        if (existing.isPresent() && existing.get().getOccupant().getId() != commonFacilityReservationDto.getOccupant().getId())
+            throw new OperationNonPermittedException("There is already a reservation at the given time slot at the name of " + commonFacilityReservationDto.getOccupant().getFirstName());
+
+        CommonFacilityReservation commonFacilityReservation = commonFacilityReservationDto.toEntity(commonFacilityReservationDto);
+        CommonFacilityReservation savedReservation = commonFacilityReservationRepository.save(commonFacilityReservation);
+        return commonFacilityReservationDto.fromEntity(savedReservation);
+    }
+
+    @Override
+    public CommonFacilityReservationDto updateReservation(CommonFacilityReservationDto commonFacilityReservationDto, Integer occupantID, Integer commonFacilityID) {
         return null;
     }
 
     @Override
-    public PublicUserDto updateReservation(CommonFacilityReservationDto commonFacilityReservationDto, Integer occupantID, Integer commonFacilityID) {
-        return null;
-    }
-
-    @Override
-    public PublicUserDto getreservation(Integer occupantID, Integer commonFacilityID) {
+    public CommonFacilityReservationDto getReservation(Integer occupantID, Integer commonFacilityID) {
         return null;
     }
 
