6a31260342f3d3c466fc9c03ccbab43d58b630ee
Abdel <abdelkader.habel_pro@outlook.com>
2024-04-13 20:58:05 -0400

    added test fixes that prevented backend build

--- rently-API/src/test/java/com/rently/rentlyAPI/entity/EmployeeAssignmentTest.java
+++ rently-API/src/test/java/com/rently/rentlyAPI/entity/EmployeeAssignmentTest.java
@@ -1,7 +1,11 @@
 package com.rently.rentlyAPI.entity;
 
+import com.rently.rentlyAPI.entity.enums.AssignmentStatus;
+import com.rently.rentlyAPI.entity.enums.WorkType;
 import com.rently.rentlyAPI.entity.user.Employee;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -11,11 +15,12 @@ public class EmployeeAssignmentTest {
     Company mockedCompany = mock(Company.class);
     Employee mockedEmployee = mock(Employee.class);
     OwnerRequest mockedOwnerRequest = mock(OwnerRequest.class);
+    List<AssignmentUpdate> nullList = null;
 
     @Test
     public void testAllArgsConstructor() {
 
-        EmployeeAssignment testEmployeeAssignment = new EmployeeAssignment(mockedCompany, mockedEmployee, mockedOwnerRequest);
+        EmployeeAssignment testEmployeeAssignment = new EmployeeAssignment(mockedCompany, mockedEmployee, mockedOwnerRequest, WorkType.GENERAL, AssignmentStatus.NOT_ASSIGNED, nullList);
         assertEquals(mockedCompany, testEmployeeAssignment.getCompany());
         assertEquals(mockedEmployee, testEmployeeAssignment.getEmployee());
         assertEquals(mockedOwnerRequest, testEmployeeAssignment.getOwnerRequest());
